85b4bf62083ff1a982753aa055a402fe085cf74a
keenonkites <keenonkites@gmx.net>
2016-12-08 19:00:49 +0100

    next try

--- tour-de-suisse/map-dev/tds-statistics.js
+++ tour-de-suisse/map-dev/tds-statistics.js
@@ -1,8 +1,1 @@
-var tdsStatistics = 
-'
-<h3>Tour de Suisse</h3>
-  <p>https://coord.info/GCQG54</p>
-  <h5>Jährliche Statistiken</h5>
-  <pre>adfadf bla bla
-</pre>
-';
+var tdsInfoHeader = '<h3>Tour de Suisse</h3><p>https://coord.info/GCQG54</p><h5>Jährliche Statistiken</h5>';
