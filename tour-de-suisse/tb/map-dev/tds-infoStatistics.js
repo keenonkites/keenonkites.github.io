ddf2cb5c373a7f38f0600937e072da0e57eb65a2
keenonkites <keenonkites@gmx.net>
2017-05-24 17:43:29 +0200

    deleted old stuff

--- tour-de-suisse/tb/map-dev/tds-infoStatistics.js
+++
@@ -1,2 +0,0 @@
-var tdsInfoHeader = '<h2>Tour de Suisse</h2><p><a target=\"_blank\" href=\"https://coord.info/GCQG54\">https://coord.info/GCQG54</a></p><pre><h3>J&auml;hrliche Statistiken</h3>';
-var tdsInfoStatistics = '2017     1 Positionen     0.000 km<br><br><h3>Gesammtstatistiken</h3>Total Positionen:            1<br>Total Kilometer:             0.000<br><br>Tage unterwegs:           4274<br>Wochen unterwegs:          611<br><br>Positionen pro Woche:        0.00<br>Kilometer pro Woche:         0.000<br>Kilometer pro Position:      0.000<br>';
