64e108a977b32830e96032215c803f1c60ec0c6e
keenonkites <keenonkites@gmx.net>
2018-01-13 21:43:41 +0100

    Adding founds to stopovers

--- tds-tb/map-dev/tds-stopovers.js
+++ tds-tb/map-dev/tds-stopovers.js
@@ -6,17 +6,19 @@ var stringStopoverDe = "Zwischenenziel ";
 
 nextStopoverCoords[nextStopoverIndex] = [47.013885, 7.959830];
 nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Änziloch");
-nextStopoverName[nextStopoverIndex]   = nextStopoverName[nextStopoverIndex].concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Änziloch");
+nextStopoverName[nextStopoverIndex]   = nextStopoverName[nextStopoverIndex].concat ( "<br>30/05/2017 icklesidi");
 stopoverIndex++;
 nextStopoverIndex++;
 
 nextStopoverCoords[nextStopoverIndex] = [47.189609, 8.175466];
 nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Blosenbergturm");
+nextStopoverName[nextStopoverIndex]   = nextStopoverName[nextStopoverIndex].concat ( "<br>16/07/2017 w-b-k-r");
 stopoverIndex++;
 nextStopoverIndex++;
 
 nextStopoverCoords[nextStopoverIndex] = [47.352014, 8.509486];
 nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Schützenhaus Albisgüetli");
+nextStopoverName[nextStopoverIndex]   = nextStopoverName[nextStopoverIndex].concat ( "<br>02/12/2017 MCAarau");
 stopoverIndex++;
 nextStopoverIndex++;
 
