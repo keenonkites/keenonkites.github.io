72b14aac5e3ad97cee0dc579f3c7b8741bd82469
keenonkites <keenonkites@gmx.net>
2018-01-13 10:09:30 +0100

    Adding external file for stopovers

---
+++ tds-tb/map-dev/tds-stopovers.js
@@ -0,0 +1,18 @@
+vars stopoverIndex, nextStopoverIndex;
+stopoverIndex = 0;
+nextStopoverIndex = 0;
+
+nextStopoverCoords[nextStopoverindex] = [47.352014, 8.509486];
+nextStopoverName[nextStopoverindex]   = "Zwischenziel 1<br>Stopover 1<br>Schützenhaus Albisgüetli";
+stopoverIndex++;
+nextStopoverIndex++;
+
+nextStopoverCoords[nextStopoverindex] = [47.696911, 8.639903];
+nextStopoverName[nextStopoverindex]   = "Zwischenziel 2<br>Stopover 2<br>Munot Schaffhausen";
+stopoverIndex++;
+nextStopoverIndex++;
+
+nextStopoverCoords[nextStopoverindex] = [47.666637, 8.981765];
+nextStopoverName[nextStopoverindex]   = "Zwischenziel 3<br>Stopover 3<br>Rathaus Steckborn";
+stopoverIndex++;
+nextStopoverIndex++;
