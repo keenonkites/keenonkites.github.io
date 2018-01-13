3e253cea58ecbcb4681a638fc85888d33eca20d6
keenonkites <keenonkites@gmx.net>
2018-01-13 19:09:35 +0100

    All stopovers

--- tds-tb/map-dev/tds-stopovers.js
+++ tds-tb/map-dev/tds-stopovers.js
@@ -4,6 +4,16 @@ var stopoverIndex = 0;
 var nextStopoverIndex = 0;
 var stringStopoverDe = "Zwischenenziel ";
 
+nextStopoverCoords[nextStopoverIndex] = [47.013885, 7.959830];
+nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Änziloch");
+stopoverIndex++;
+nextStopoverIndex++;
+
+nextStopoverCoords[nextStopoverIndex] = [47.189609, 8.175466];
+nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Blosenbergturm");
+stopoverIndex++;
+nextStopoverIndex++;
+
 nextStopoverCoords[nextStopoverIndex] = [47.352014, 8.509486];
 nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Schützenhaus Albisgüetli");
 stopoverIndex++;
@@ -18,3 +28,12 @@ nextStopoverCoords[nextStopoverIndex] = [47.666637, 8.981765];
 nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Rathaus Steckborn");
 stopoverIndex++;
 nextStopoverIndex++;
+
+nextStopoverCoords[nextStopoverIndex] = [47.559705, 9.377272];
+nextStopoverName[nextStopoverIndex]   = stringStopoverDe.concat ( nextStopoverIndex + 1, "<br>Stopover ", nextStopoverIndex + 1, "<br>Dampflock Locorama Romanshorn");
+stopoverIndex++;
+nextStopoverIndex++;
+
+
+
+
